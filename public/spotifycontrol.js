c69427d95d710c9ff97399708b005098176654d1
Mikko Heikkinen <mizheikk@gmail.com>
2014-12-14 14:28:14 +0200

    Info päivittyy kerran sekunnissa. Volume napit toimivat.

--- public/spotifycontrol.js
+++ public/spotifycontrol.js
@@ -1,66 +1,61 @@
 $( document ).ready(function() {
  	
- 	//var url = "http://omena.aappala.fi:8080/command";
- 	var url = "http://localhost:8080/command";
- 	var fadeoutTime = 5000;
- 	var fadeinTime = 200;
+ 	var url = "http://localhost:8080/command"; 	
+ 	var volume = 0;
+	
+	/* Hakee kappaletiedot kerran sekunnissa.
+	 * TODO hae tiedot vain kun oikeasti tarvitsee..
+	*/
+	
+	function infoLoop() {
+    $.get(url, { command:"info"}, function(data){
+  		var info = $.parseJSON(data);
+  		$("#info").html(info.artist + "<br/>" +
+  			              info.track  + "<br/>" +
+  			              info.album  + "<br/>" +
+  			              info.nowAt  
+  		);
+  		
+  		volume = info.volume;
+		});			
+	}
+	setInterval(infoLoop, 1000);
+	
 
 	$("#step-backward").click(function(){
 		$.get(url, { command:"prev"}, function(data){
-			$( "#info" ).fadeIn( fadeinTime );
-  		$("#info").html(data);
-  		$( "#info" ).fadeOut( fadeoutTime );
 		});
 	});	
 
 	$("#play").click(function(){
 		$.get(url, { command:"play"}, function(data){
-			$( "#info" ).fadeIn( fadeinTime );
-  		$("#info").html(data);
-  		$( "#info" ).fadeOut( fadeoutTime );
 		});
 	});	
 
 	$("#pause").click(function(){
 		$.get(url, { command:"pause"}, function(data){
-			$( "#info" ).fadeIn( fadeinTime );
-  		$("#info").html(data);
-  		$( "#info" ).fadeOut( fadeoutTime );
 		});
 	});	
 	
 	$("#step-forward").click(function(){
 		$.get(url, { command:"next"}, function(data){
-			$( "#info" ).fadeIn( fadeinTime );
-  		$("#info").html(data);
-  		$( "#info" ).fadeOut( fadeoutTime );
 		});
 	});		
 	
 	$("#volume-down").click(function(){
-		$.get(url, { command:"volume", param:"10"}, function(data){
-			$( "#info" ).fadeIn( fadeinTime );
-  		$("#info").html(data);
-  		$( "#info" ).fadeOut( 10000 );
-		});
+		if(volume > 0) {
+			volume = volume - 10;
+		  $.get(url, { command:"volume", param:volume}, function(data){
+		  });
+		}
 	});
 
 	$("#volume-up").click(function(){
-		$.get(url, { command:"volume", param:"100"}, function(data){
-			$( "#info" ).fadeIn( fadeinTime );
-  		$("#info").html(data);
-  		$( "#info" ).fadeOut( 10000 );
-		});
-	});		
-	
-	$("#heart").click(function(){
-		$.get(url, { command:"info"}, function(data){
-			$( "#info" ).fadeIn( fadeinTime );
-  		$("#info").html(data);
-  		var perse = $.parseJSON(data);
-  		console.log(perse.artist);
-  		$( "#info" ).fadeOut( 10000 );
-		});
-	});			
-		 
+		if(volume < 100) {
+			volume = volume + 10;
+		  $.get(url, { command:"volume", param:volume}, function(data){
+		  });
+		}
+	});	
+			 
 });
